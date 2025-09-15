e252113885587b966664ef20af2e2643e3c12840
Justin Iaconis <ohiorules123@gmail.com>
2025-09-14 20:52:21 -0700

    Create SoloTempInvItemOverlay.java

--- src/main/java/com/yostomabagel/runelite/solotemp/SoloTempInvItemOverlay.java
+++ src/main/java/com/yostomabagel/runelite/solotemp/SoloTempInvItemOverlay.java
@@ -4,12 +4,17 @@ import java.awt.Graphics2D;
 
 import javax.inject.Inject;
 
+import net.runelite.api.ItemContainer;
 import net.runelite.api.widgets.WidgetItem;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.ui.overlay.WidgetItemOverlay;
 
 public class SoloTempInvItemOverlay extends WidgetItemOverlay
 {
+	private static final int HARPOONFISHRAW_ID = 25564;
+	private static final int HARPOONFISHCOOKED_ID = 25565;
+	private static final int WATERBUCKET_ID = 1929;
+	
 	private final ItemManager itemManager;
 	private final SoloTempPlugin plugin;
 	private final SoloTempConfig config;
@@ -25,9 +30,24 @@ public class SoloTempInvItemOverlay extends WidgetItemOverlay
 	
 	
 	@Override
-	public void renderItemOverlay(Graphics2D graphics, int itemId, WidgetItem widgetItem) {
-		// TODO Auto-generated method stub
+	public void renderItemOverlay(Graphics2D graphics, int itemId, WidgetItem widgetItem)
+	{
+		ItemContainer inventory = plugin.getPlayerInv();
+		if (inventory == null) return;
 		
+		int harpRawCount = 0;
+		int harpCookCount = 0;
+		int otherItemCount = 0;
+		int emptyItemCount = 0;
+		
+		for (int i = 0; i < inventory.getItems().length; i++) 
+		{
+			if (inventory.getItems()[i].getId() == HARPOONFISHRAW_ID) 
+			{
+				harpRawCount++;
+				
+			}
+		}
 	}
 	
 }
